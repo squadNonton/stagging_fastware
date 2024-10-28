1a271d8b0ae5249e6310e464af544b9b7e25ad85
Riansryn <riansuryana056@gmail.com>
2024-10-28 13:43:24 +0700

    CheckBox FPB FINN

--- resources/views/po_pengajuan/index_po_finance.blade.php
+++ resources/views/po_pengajuan/index_po_finance.blade.php
@@ -17,7 +17,7 @@
         <section class="section">
             <div class="row">
                 <div class="col-lg-12">
-
+                    
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Tampilan Data Form Permintaan Barang/Jasa</h5>
